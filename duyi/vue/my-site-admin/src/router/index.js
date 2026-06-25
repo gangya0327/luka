7b8ec79a51e3eb6ac46ad9e32b639c25ce90bd4c
gangya0327 <king9999427@163.com>
2026-06-26 02:39:07 +0800

    ✨ feat: 更新路由配置，添加多个页面组件，包括首页、文章管理、项目管理和设置

--- duyi/vue/my-site-admin/src/router/index.js
+++ duyi/vue/my-site-admin/src/router/index.js
@@ -51,111 +51,121 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '首页', icon: 'dashboard' }
     }]
   },
 
   {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
+    path: '/banner',
     component: Layout,
     children: [
       {
         path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        name: 'Banner',
+        component: () => import('@/views/banner/index'),
+        meta: { title: '首页轮播', icon: 'el-icon-picture', auth: true }
       }
     ]
   },
 
   {
-    path: '/nested',
+    path: '/blog',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
+    redirect: '/blog/list',
+    name: 'Example',
+    meta: { title: '文章管理', icon: 'el-icon-s-order' },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+        path: 'list',
+        name: 'BlogList',
+        component: () => import('@/views/blog/list'),
+        meta: { title: '文章列表', icon: 'el-icon-document', auth: true }
       },
       {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        path: 'type',
+        name: 'BlogType',
+        component: () => import('@/views/blog/type'),
+        meta: { title: '文章分类', icon: 'el-icon-tickets', auth: true }
+      },
+      {
+        path: 'add',
+        name: 'BlogAdd',
+        component: () => import('@/views/blog/add'),
+        meta: { title: '添加文章', icon: 'el-icon-document-add', auth: true }
       }
     ]
   },
 
   {
-    path: 'external-link',
+    path: '/project',
+    component: Layout,
+    meta: { title: ' 项目管理', icon: 'el-icon-s-cooperation', auth: true },
+    redirect: '/project/list',
+    name: ' Project',
+    children: [
+      {
+        path: 'list',
+        name: ' ProjectList',
+        component: () => import('@/views/project/list'),
+        meta: { title: '项目列表', icon: 'el-icon-suitcase', auth: true }
+      },
+      {
+        path: 'add',
+        name: ' ProjectAdd',
+        component: () => import('@/views/project/add'),
+        meta: { title: '添加项目', icon: 'el-icon-first-aid-kit', auth: true }
+      }
+    ]
+  },
+
+  {
+    path: '/comment',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'index',
+        name: 'Comment',
+        component: () => import('@/views/comment/index'),
+        meta: { title: '评论管理', icon: 'el-icon-s-comment', auth: true }
+      }
+    ]
+  },
+
+  {
+    path: '/message',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Message',
+        component: () => import('@/views/message/index'),
+        meta: { title: '留言板', icon: 'el-icon-s-claim', auth: true }
+      }
+    ]
+  },
+
+  {
+    path: '/about',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'About',
+        component: () => import('@/views/about/index'),
+        meta: { title: '关于我', icon: 'el-icon-user-solid', auth: true }
+      }
+    ]
+  },
+
+  {
+    path: '/setting',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Setting',
+        component: () => import('@/views/setting/index'),
+        meta: { title: '设置', icon: 'el-icon-s-tools', auth: true }
       }
     ]
   },
---
+++ duyi/vue/my-site-admin/src/views/banner/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    轮播列表
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
