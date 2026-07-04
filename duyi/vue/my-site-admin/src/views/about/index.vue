cc5d9fd84ead141464a83f3bc2b457c3d9df1443
gangya0327 <king9999427@163.com>
2026-07-04 16:06:11 +0800

    ✨ feat: 添加关于和设置功能的API接口

--- duyi/vue/my-site-admin/src/views/about/index.vue
+++ duyi/vue/my-site-admin/src/views/about/index.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-    关于列表
+  <div class="app-container">
+    <div class="block"> 关于我</div>
+    <el-input v-model="url" placeholder="请输入内容" :disabled="disabled" />
+    <el-button type="primary" style="margin-top: 15px" @click="handleClick">{{
+      btnText
+    }}</el-button>
   </div>
 </template>
 
 <script>
-export default {
+import { getAbout, editAbout } from '@/api/about'
 
+export default {
+  data() {
+    return {
+      url: '',
+      disabled: false,
+      btnText: '修改'
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      getAbout().then((res) => {
+        this.url = res.data
+      })
+    },
+    handleClick() {
+      if (this.btnText === '修改') {
+        this.btnText = '完成'
+        this.disabled = !this.disabled
+      } else {
+        if (this.url) {
+          this.btnText = '修改'
+          this.disabled = !this.disabled
+          editAbout({ url: this.url }).then((res) => {
+            this.$message.success('修改成功')
+          })
+        } else {
+          this.$message.error('请输入内容')
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.block {
+  margin: 15px 0;
+  font-weight: 100;
+}
 </style>
--- duyi/vue/my-site-admin/src/views/setting/index.vue
+++ duyi/vue/my-site-admin/src/views/setting/index.vue
@@ -1,15 +1,126 @@
 <template>
-  <div>
-    设置列表
+  <div class="app-container">
+    <div class="block">网站信息</div>
+    <div class="block">网站标题</div>
+    <div style="margin-bottom: 15px">
+      <el-input v-model="form.siteTitle" disabled />
+    </div>
+
+    <div class="block">邮箱</div>
+    <div style="margin-bottom: 15px">
+      <el-input v-model="form.mail" disabled />
+    </div>
+
+    <div class="block">备案号</div>
+    <div style="margin-bottom: 15px">
+      <el-input v-model="form.icp" disabled />
+    </div>
+
+    <el-divider />
+
+    <div class="block">网站头像信息</div>
+    <div style="margin-bottom: 15px">
+      <el-image :src="form.avatar2" style="width: 100px; height: 100px" />
+    </div>
+
+    <el-divider />
+
+    <div class="block">网站图标信息</div>
+    <div class="block">网站图标地址</div>
+    <div style="margin-bottom: 15px">
+      <el-input v-model="form.favicon" disabled />
+    </div>
+
+    <div class="block">网站图标预览</div>
+    <el-image v-model="form.favicon" disabled />
+
+    <el-divider />
+
+    <div class="block">github信息</div>
+    <div class="block">github名称</div>
+    <el-input v-model="form.githubName" placeholder="请输入github名称" disabled />
+
+    <div class="block">github地址</div>
+    <el-input v-model="form.github" placeholder="请输入github地址" disabled />
+
+    <el-divider />
+
+    <div class="block">QQ信息</div>
+    <div class="block">QQ号码</div>
+    <el-input v-model="form.qq" disabled />
+
+    <div class="block">QQ 二维码预览</div>
+    <el-image v-model="form.qqQrCode2" disabled />
+
+    <div class="block">微信信息</div>
+    <div class="block">微信号码</div>
+    <el-input v-model="form.weixin" disabled />
+
+    <div class="block">微信二维码预览</div>
+    <el-image v-model="form.weixinQrCode2" disabled />
+
+    <el-divider />
+
+    <div class="block">
+      <el-button type="primary" @click="handleEditMode">进入编辑模式</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { getSetting } from '@/api/setting'
+// import Upload from '@/components/Upload'
+import { server_url } from '@/utils/config'
 
+export default {
+  // components: { Upload },
+  data() {
+    return {
+      server_url,
+      dialogVisible: false,
+      form: {
+        siteTitle: '',
+        mail: '',
+        icp: '',
+        avatar: '',
+        avatar2: '',
+        url: '',
+        favicon: '',
+        githubName: '',
+        github: '',
+        qq: '',
+        qqQrCode2: '',
+        weixin: '',
+        weixinQrCode2: ''
+      }
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      getSetting().then((res) => {
+        this.form = res.data
+        this.form.avatar2 = this.server_url + this.form.avatar
+        this.form.favicon = this.server_url + this.form.favicon
+      })
+    },
+    handleEditMode() {
+      this.dialogVisible = true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.app-container {
+  width: 800px;
+  margin: 0 auto;
+}
 
+.block {
+  margin: 15px 0;
+  font-weight: 100;
+}
 </style>
